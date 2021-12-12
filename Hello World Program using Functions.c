e752cb3b37ff42288b7c5517ced02b81877901f9
SumonM <sumon.wdev@gmail.com>
2021-12-12 10:32:05 +0600

    Hello World Program using Functions

---
+++ Hello World Program using Functions.c
@@ -0,0 +1,15 @@
+/*
+ *Example 2: Hello World Program using Functions
+ *Lets see the same program using user defined function. In this program we have created a function hello() that prints the message on the screen. We are calling this function in the main() function. To understand user defined functions, read this guide: User Defined Function in C.
+*/
+
+#include <stdio.h>
+void hello(){
+	printf("Hello World");
+}
+int main()
+{
+   //Calling a function here
+   hello();
+   return 0;
+}
